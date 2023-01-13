26c5c3602ccae857db8073917648d6a1a09185cc
Erick Hernandez <e.hernandezr16@gmail.com>
2023-01-13 14:04:19 -0600

    Added functions for creating and compressing folders, also to check if file/folder exists

--- modules/file_handler.py
+++ modules/file_handler.py
@@ -1,3 +1,6 @@
+import os
+import tarfile
+
 def read_text_file(filepath:str) -> str:
     
     try:
@@ -9,3 +12,34 @@ def read_text_file(filepath:str) -> str:
             exit()
     
     return text
+
+def create_folder(directory):
+
+    parent_directory = ""
+    path = os.path.join(parent_directory, directory)
+
+    if os.path.exists(path):
+        return
+
+    try:
+        os.mkdir(path)
+    except OSError as error:
+        print(error)
+        exit()
+
+def compress_folder(filepath:str, output_filename:str):
+    if not os.path.exists(filepath):
+        print('Folder not found')
+        exit()
+
+    files = os.listdir(filepath)
+
+    if len(files) == 0:
+        return
+
+    with tarfile.open(output_filename, "w:gz") as tar:
+        for file in files:
+            tar.add(filepath+'/'+file)
+
+def file_exists(filepath:str):
+    return os.path.exists(filepath)
