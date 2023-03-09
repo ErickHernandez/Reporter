bea1eb866f602ce0373357784c204a21b7a45865
Erick Hernandez <e.hernandezr16@gmail.com>
2023-03-08 19:51:51 -0600

    Added compression function

--- modules/file_handler.py
+++ modules/file_handler.py
@@ -1,20 +1,19 @@
 import os
 import tarfile
 
-def read_text_file(filepath:str) -> str:
-    
-    try:
 
+def read_text_file(filepath: str) -> str:
+    try:
         f = open(filepath, "r")
         text = f.read()
     except FileNotFoundError as e:
-            print("File '{}' not found.".format(filepath))
-            exit()
-    
+        print("File '{}' not found.".format(filepath))
+        exit()
+
     return text
 
-def create_folder(directory):
 
+def create_folder(directory):
     parent_directory = ""
     path = os.path.join(parent_directory, directory)
 
@@ -27,9 +26,10 @@ def create_folder(directory):
         print(error)
         exit()
 
-def compress_folder(filepath:str, output_filename:str):
+
+def compress_folder(filepath: str, output_filename: str):
     if not os.path.exists(filepath):
-        print('Folder not found')
+        print("Folder not found")
         exit()
 
     files = os.listdir(filepath)
@@ -39,7 +39,8 @@ def compress_folder(filepath:str, output_filename:str):
 
     with tarfile.open(output_filename, "w:gz") as tar:
         for file in files:
-            tar.add(filepath+'/'+file)
+            tar.add(filepath + "/" + file)
 
-def file_exists(filepath:str):
+
+def file_exists(filepath: str):
     return os.path.exists(filepath)
